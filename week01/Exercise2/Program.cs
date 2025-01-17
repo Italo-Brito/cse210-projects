540d1fc79b41b386987f952206d095fb53c120b7
Italo-Brito <italo.family@gmail.com>
2025-01-17 13:11:54 -0300

    Letter grade

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,48 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What is your grade porcentage? ");
+        string unserInput =  Console.ReadLine();
+        float grade = float.Parse(unserInput);
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congrats, you passed!!!!");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately you didn't get enough grade to pass, please try again next time.");
+        }
     }
+    
 }
