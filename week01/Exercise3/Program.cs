e3149254d79d26e1c818366210a2467ead1010a9
Italo-Brito <italo.family@gmail.com>
2025-03-14 22:12:38 -0300

    Exercice3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Hello World! This is the Exercise 3 Project.");
+
+        Console.Write("What is the magic number? ");
+        string lnumber = Console.ReadLine();
+        float number = float.Parse(lnumber);
+
+        float gnumber = -1;
+        string lgnumber;
+
+        while(gnumber != number)
+        {
+            Console.Write("What is your guess?");
+            lgnumber = Console.ReadLine();
+            gnumber = float.Parse(lgnumber);  
+
+            if (gnumber > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (gnumber < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You Guessed it!");
+            }
+        }
+
+        
+
     }
 }
