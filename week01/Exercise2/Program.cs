1bb67fa8440adedae0271bf1c5186c12785b62ac
Italo-Brito <italo.family@gmail.com>
2025-03-14 21:18:14 -0300

    exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Hello World! This is the Exercise 2 Project.");
 
         Console.Write("What is your grade porcentage? ");
         string unserInput =  Console.ReadLine();
